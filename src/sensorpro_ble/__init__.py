92b1927499b35586e54bf3d2d7026ea1e69b0b55
J. Nick Koston <nick@koston.org>
2022-08-30 16:01:08 -0500

    feat: init repo

--- src/sensorpro_ble/__init__.py
+++ src/sensorpro_ble/__init__.py
@@ -1,1 +1,32 @@
-__version__ = "0.0.1"
+"""Parser for SensorPro BLE advertisements."""
+from __future__ import annotations
+
+from sensor_state_data import (
+    BinarySensorDeviceClass,
+    BinarySensorValue,
+    DeviceKey,
+    SensorDescription,
+    SensorDeviceClass,
+    SensorDeviceInfo,
+    SensorUpdate,
+    SensorValue,
+    Units,
+)
+
+from .parser import SensorProBluetoothDeviceData
+
+__version__ = "0.3.0"
+
+__all__ = [
+    "SensorProBluetoothDeviceData",
+    "BinarySensorDeviceClass",
+    "BinarySensorValue",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceClass",
+    "SensorDeviceInfo",
+    "SensorValue",
+    "Units",
+]
