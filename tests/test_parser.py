8235d01f291e34e2cbbdce9e6dd7a888297ac84e
J. Nick Koston <nick@koston.org>
2022-08-30 17:00:43 -0500

    feat: add support for the t201/t301 (#5)

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,8 +1,5 @@
 from bluetooth_sensor_state_data import BluetoothServiceInfo, SensorUpdate
 from sensor_state_data import (
-    BinarySensorDescription,
-    BinarySensorDeviceClass,
-    BinarySensorValue,
     DeviceKey,
     SensorDescription,
     SensorDeviceClass,
@@ -18,67 +15,136 @@ def test_can_create():
     SensorProBluetoothDeviceData()
 
 
-MFR_20 = BluetoothServiceInfo(
-    name="SensorPro",
+MFR_T201 = BluetoothServiceInfo(
+    name="T201",
     address="aa:bb:cc:dd:ee:ff",
     rssi=-60,
     service_data={},
     manufacturer_data={
-        16: b"\x00\x00\xb0\x02\x00\x00G\xa4\xe2\x0c\x80\x01\xb6\x02J\x00\x00\x00"
+        43605: b"\x01\x01\xa4\xc18.\xcan\x01\x07\n\x02\x13\x9dd\x00\x01\x01\x01\xa4\xc18.\xcan\x01\x07\n\x02\x13\x9dd\x00\x01"
     },
-    service_uuids=["0000fff0-0000-1000-8000-00805f9b34fb"],
+    service_uuids=[],
     source="local",
 )
 
-MFR_22 = BluetoothServiceInfo(
-    name="SensorPro",
+MFR_T301 = BluetoothServiceInfo(
+    name="T301",
     address="aa:bb:cc:dd:ee:ff",
     rssi=-60,
     service_data={},
-    manufacturer_data={
-        21: b"\x00\x00\xf0\x05\x00\x00\xd7n\xbe\x01e\x00\x00\x00\xa7\x01\x00\x00\x00\x00"
-    },
-    service_uuids=["0000fff0-0000-1000-8000-00805f9b34fb"],
+    manufacturer_data={43605: b"\x01\x05\xa4\xc18\x1aWv\x01\x07\tV\x17\x0ca\x00\x01"},
+    service_uuids=[],
+    source="local",
+)
+
+MFR_T301_NO_NAME = BluetoothServiceInfo(
+    name=None,
+    address="aa:bb:cc:dd:ee:ff",
+    rssi=-60,
+    service_data={},
+    manufacturer_data={43605: b"\x01\x05\xa4\xc18\x1aWv\x01\x07\tV\x17\x0ca\x00\x01"},
+    service_uuids=[],
     source="local",
 )
 
 
-def test_with_22_byte_update():
+def test_t201():
     parser = SensorProBluetoothDeviceData()
-    parser.supported(MFR_22) is True
-    assert parser.title == "Smart hygrometer EEFF"
-
-
-def test_supported_set_the_title():
-    parser = SensorProBluetoothDeviceData()
-    parser.supported(MFR_20) is True
-    assert parser.title == "Lanyard/mini hygrometer EEFF"
-
-
-def test_20_byte_update():
-    parser = SensorProBluetoothDeviceData()
-    update = parser.update(MFR_20)
+    update = parser.update(MFR_T201)
     assert update == SensorUpdate(
-        title="Lanyard/mini hygrometer EEFF",
+        title="T201 EEFF",
         devices={
             None: SensorDeviceInfo(
-                name="Lanyard/mini hygrometer EEFF",
-                model=16,
+                name="T201 EEFF",
+                model=1,
                 manufacturer="SensorPro",
                 sw_version=None,
                 hw_version=None,
             )
         },
         entity_descriptions={
+            DeviceKey(key="voltage", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="voltage", device_id=None),
+                device_class=SensorDeviceClass.VOLTAGE,
+                native_unit_of_measurement=Units.ELECTRIC_POTENTIAL_VOLT,
+            ),
             DeviceKey(key="humidity", device_id=None): SensorDescription(
                 device_key=DeviceKey(key="humidity", device_id=None),
                 device_class=SensorDeviceClass.HUMIDITY,
                 native_unit_of_measurement=Units.PERCENTAGE,
             ),
+            DeviceKey(key="signal_strength", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="signal_strength", device_id=None),
+                device_class=SensorDeviceClass.SIGNAL_STRENGTH,
+                native_unit_of_measurement=Units.SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+            ),
+            DeviceKey(key="battery", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="battery", device_id=None),
+                device_class=SensorDeviceClass.BATTERY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            DeviceKey(key="temperature", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="temperature", device_id=None),
+                device_class=SensorDeviceClass.TEMPERATURE,
+                native_unit_of_measurement=Units.TEMP_CELSIUS,
+            ),
+        },
+        entity_values={
+            DeviceKey(key="voltage", device_id=None): SensorValue(
+                device_key=DeviceKey(key="voltage", device_id=None),
+                name="Voltage",
+                native_value=2.63,
+            ),
+            DeviceKey(key="humidity", device_id=None): SensorValue(
+                device_key=DeviceKey(key="humidity", device_id=None),
+                name="Humidity",
+                native_value=50.21,
+            ),
+            DeviceKey(key="signal_strength", device_id=None): SensorValue(
+                device_key=DeviceKey(key="signal_strength", device_id=None),
+                name="Signal " "Strength",
+                native_value=-60,
+            ),
+            DeviceKey(key="battery", device_id=None): SensorValue(
+                device_key=DeviceKey(key="battery", device_id=None),
+                name="Battery",
+                native_value=100,
+            ),
+            DeviceKey(key="temperature", device_id=None): SensorValue(
+                device_key=DeviceKey(key="temperature", device_id=None),
+                name="Temperature",
+                native_value=25.62,
+            ),
+        },
+        binary_entity_descriptions={},
+        binary_entity_values={},
+    )
+
+
+def test_t301():
+    parser = SensorProBluetoothDeviceData()
+    update = parser.update(MFR_T301)
+    assert update == SensorUpdate(
+        title="T301 EEFF",
+        devices={
+            None: SensorDeviceInfo(
+                name="T301 EEFF",
+                model=5,
+                manufacturer="SensorPro",
+                sw_version=None,
+                hw_version=None,
+            )
+        },
+        entity_descriptions={
             DeviceKey(key="voltage", device_id=None): SensorDescription(
                 device_key=DeviceKey(key="voltage", device_id=None),
                 device_class=SensorDeviceClass.VOLTAGE,
                 native_unit_of_measurement=Units.ELECTRIC_POTENTIAL_VOLT,
+            ),
+            DeviceKey(key="humidity", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="humidity", device_id=None),
+                device_class=SensorDeviceClass.HUMIDITY,
+                native_unit_of_measurement=Units.PERCENTAGE,
             ),
             DeviceKey(key="temperature", device_id=None): SensorDescription(
                 device_key=DeviceKey(key="temperature", device_id=None),
@@ -97,25 +163,25 @@ def test_20_byte_update():
             ),
         },
         entity_values={
-            DeviceKey(key="humidity", device_id=None): SensorValue(
-                device_key=DeviceKey(key="humidity", device_id=None),
-                name="Humidity",
-                native_value=43.38,
-            ),
             DeviceKey(key="voltage", device_id=None): SensorValue(
                 device_key=DeviceKey(key="voltage", device_id=None),
                 name="Voltage",
-                native_value=3.3,
+                native_value=2.63,
+            ),
+            DeviceKey(key="humidity", device_id=None): SensorValue(
+                device_key=DeviceKey(key="humidity", device_id=None),
+                name="Humidity",
+                native_value=59.0,
             ),
             DeviceKey(key="temperature", device_id=None): SensorValue(
                 device_key=DeviceKey(key="temperature", device_id=None),
                 name="Temperature",
-                native_value=24.0,
+                native_value=23.9,
             ),
             DeviceKey(key="battery", device_id=None): SensorValue(
                 device_key=DeviceKey(key="battery", device_id=None),
                 name="Battery",
-                native_value=100,
+                native_value=97,
             ),
             DeviceKey(key="signal_strength", device_id=None): SensorValue(
                 device_key=DeviceKey(key="signal_strength", device_id=None),
@@ -123,17 +189,79 @@ def test_20_byte_update():
                 native_value=-60,
             ),
         },
-        binary_entity_descriptions={
-            DeviceKey(key="occupancy", device_id=None): BinarySensorDescription(
-                device_key=DeviceKey(key="occupancy", device_id=None),
-                device_class=BinarySensorDeviceClass.OCCUPANCY,
+        binary_entity_descriptions={},
+        binary_entity_values={},
+    )
+
+
+def test_t301_passive():
+    parser = SensorProBluetoothDeviceData()
+    update = parser.update(MFR_T301_NO_NAME)
+    assert update == SensorUpdate(
+        title="T301 EEFF",
+        devices={
+            None: SensorDeviceInfo(
+                name="T301 EEFF",
+                model=5,
+                manufacturer="SensorPro",
+                sw_version=None,
+                hw_version=None,
             )
         },
-        binary_entity_values={
-            DeviceKey(key="occupancy", device_id=None): BinarySensorValue(
-                device_key=DeviceKey(key="occupancy", device_id=None),
-                name="Occupancy",
-                native_value=False,
-            )
+        entity_descriptions={
+            DeviceKey(key="voltage", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="voltage", device_id=None),
+                device_class=SensorDeviceClass.VOLTAGE,
+                native_unit_of_measurement=Units.ELECTRIC_POTENTIAL_VOLT,
+            ),
+            DeviceKey(key="humidity", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="humidity", device_id=None),
+                device_class=SensorDeviceClass.HUMIDITY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            DeviceKey(key="temperature", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="temperature", device_id=None),
+                device_class=SensorDeviceClass.TEMPERATURE,
+                native_unit_of_measurement=Units.TEMP_CELSIUS,
+            ),
+            DeviceKey(key="battery", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="battery", device_id=None),
+                device_class=SensorDeviceClass.BATTERY,
+                native_unit_of_measurement=Units.PERCENTAGE,
+            ),
+            DeviceKey(key="signal_strength", device_id=None): SensorDescription(
+                device_key=DeviceKey(key="signal_strength", device_id=None),
+                device_class=SensorDeviceClass.SIGNAL_STRENGTH,
+                native_unit_of_measurement=Units.SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
+            ),
         },
+        entity_values={
+            DeviceKey(key="voltage", device_id=None): SensorValue(
+                device_key=DeviceKey(key="voltage", device_id=None),
+                name="Voltage",
+                native_value=2.63,
+            ),
+            DeviceKey(key="humidity", device_id=None): SensorValue(
+                device_key=DeviceKey(key="humidity", device_id=None),
+                name="Humidity",
+                native_value=59.0,
+            ),
+            DeviceKey(key="temperature", device_id=None): SensorValue(
+                device_key=DeviceKey(key="temperature", device_id=None),
+                name="Temperature",
+                native_value=23.9,
+            ),
+            DeviceKey(key="battery", device_id=None): SensorValue(
+                device_key=DeviceKey(key="battery", device_id=None),
+                name="Battery",
+                native_value=97,
+            ),
+            DeviceKey(key="signal_strength", device_id=None): SensorValue(
+                device_key=DeviceKey(key="signal_strength", device_id=None),
+                name="Signal " "Strength",
+                native_value=-60,
+            ),
+        },
+        binary_entity_descriptions={},
+        binary_entity_values={},
     )
